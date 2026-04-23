f78d53430817503fcde9b7e3ce3cb12a36299e09
dragunkaja <kaja.dragun@gmail.com>
2026-04-23 14:05:03 +0200

    GameBoard.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,34 @@
 <script setup>
+import { ref } from 'vue'
 
+
+const count = ref(0)
+function increment(num) {
+  count.value+=num
+}
+
+const decrement = () => {
+  count.value--
+}
+
+decrement()
 
 </script>
 
+
+
 <template>
   
-  
+  <h1>Mój pierwszy licznik Vue!</h1>
+  <button @click="decrement()" id='button123'>
+    Licznik to: {{ count }}
+  </button>
+
 </template>
 
 <style scoped>
-
+#button123 {
+  font-weight: bold;
+  background-color: #42b983;
+}
  </style>

6f5997d3dc530ecb119d7e1aed22a6b9059f3836
dragunkaja <kaja.dragun@gmail.com>
2026-04-23 20:52:55 +0200

    knm

---
+++ frontend/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue'
+import GameBoard from './components/GameBoard.vue'
+import Row from './components/Row.vue';
+
+</script>
+
+
+<template>
+  <div class="app-container">
+    
+    <GameBoard />
+    
+  </div>
+</template>
+
+<style scoped>
+/* Trochę podstawowego wyglądu, żeby gra była na środku ekranu */
+.app-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-family: sans-serif;
+  margin-top: 50px;
+}
+</style>
